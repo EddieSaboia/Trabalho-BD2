f4697c9c97cff96780fdfca5000fa2a848548b41
Eddie Saboia <eddie.neto@me.com>
2020-04-27 19:11:28 -0300

    mudança de layout e recebendo o request

--- src/Component/Textfield/index.js
+++ src/Component/Textfield/index.js
@@ -1,44 +1,61 @@
-import React from 'react';
+import React, { Component } from "react";
 
 class TextFileReader extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			text: ""
-		};
-	}
+    this.state = {
+      text: "",
+      json: new Map(),
+    };
+  }
 
-	componentDidMount() {
-		this.readTextFile(this.props.txt);
-	}
+  componentDidMount() {
+    this.readTextFile(this.props.txt);
+  }
 
-	readTextFile = file => {
-		var rawFile = new XMLHttpRequest();
-		rawFile.open("GET", file, false);
-		rawFile.onreadystatechange = () => {
-			if (rawFile.readyState === 4) {
-				if (rawFile.status === 200 || rawFile.status == 0) {
+  readTextFile = (file) => {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = () => {
+      if (rawFile.readyState === 4) {
+        if (rawFile.status === 200 || rawFile.status == 0) {
 					var allText = rawFile.responseText;
-					this.setState({
-						text: allText
-					});
-				}
-			}
-		};
-		rawFile.send(null);
-	};
+					console.log("olha eu aqui")
+          this.setState({
+            text: allText,
+          });
+        }
+      }
+    };
+    rawFile.send(null);
+  };
 
-	render() {
-		return (
-			<div>
-				{this.state.text.split("\n").map((item, key) => {
-					console.log("a",key, item)
-					
-				})}
-			</div>
-		);
-	}
+  handleSaveToPC = (jsonData) => {
+    if (jsonData.size > 0) {
+      var fileData = JSON.stringify(jsonData);
+      var blob = new Blob([fileData], { type: "text/plain" });
+      var url = URL.createObjectURL(blob);
+      var link = document.createElement("a");
+      link.download = "filename.json";
+      link.href = url;
+      link.click();
+    }
+  };
+
+  render() {
+    var a = new Map();
+
+    return (
+      <div>
+        {this.state.text.split("\n").map((item, key) => {
+						a.set(key, item);
+					}),
+					console.log(a)
+				}
+      </div>
+    );
+  }
 }
 
 export default TextFileReader;
--- src/Estruturas/Tabela/index.js
+++ src/Estruturas/Tabela/index.js
@@ -1,37 +1,125 @@
 import React, { useState } from "react";
 import Table from "../../Component/Table"
+import axios from 'axios'
+import { Form, Input, Button, Checkbox } from 'antd';
+import { FormInstance } from 'antd/lib/form';
+
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+
 
 const Tabela = () => {
-  
+
   const [state, setState] = useState({
-    buckets: []
+    buckets: [],
+    overflowTax: "",
+    colisaoTax: ""
   })
+
+  async function makeRequest(values) {
+    // POST request using fetch with async/await
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(values)
+    };
+    const response = await fetch('http://localhost:3333/manda', requestOptions);
+    const data = await response.json();
+    setState({
+      ...state,
+      buckets:data.tabela,
+      overflowTax: data.overflowTax + "%",
+      colisaoTax: data.colisaoTax + "%"
+    })
+    console.log("response", data)
+}
+
+  // const makeRequest = () => {
+  //   axios.get('https://api.github.com/users/maecapozzi')
+  //   .then(response => console.log(response))
+  // }
 
   const columns = [
     {
-      id: "word",
-      label: "word",
+      id: "index",
+      label: "endereço Bucket",
       minWidth: 150
     },
     {
-      id: "bucket",
-      label: "endereco bucket",
+      id: "paginaId",
+      label: "Pagina index",
       minWidth: 150
     },
     {
-      id: "keySearch",
-      label: "Chave de busca",
+      id: "element",
+      label: "Palavras",
+      minWidth: 150
+    },
+    {
+      id: "overflow",
+      label: "overflow",
       minWidth: 150
     }
   ]
 
+  const onFinish = values => {
+    makeRequest(values)
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
+
+
   return (
     <>
     <h1>puefhep</h1>
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Tamanho do Bucket"
+        name="tamanhoBucket"
+        rules={[{ required: true, message: 'Colocar o tamanho do bucket!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="tamanho da pagina"
+        name="tamanhoPagina"
+        // rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+
+  <p>Taxa de Overflow : {state.overflowTax} </p>
+
+  <p>Taxa de colisão : {state.colisaoTax} </p>
+
+
     <Table
     columns={columns}
-    data={[]}
+    data={state.buckets}
     />
+
     </>
   )
 
